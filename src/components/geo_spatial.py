2b3ca4126896ad486caf5e261f8f61d66492a2bd
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-09-01 20:48:51 +0200

    Match stops to districts within/close

--- src/components/geo_spatial.py
+++ src/components/geo_spatial.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentError
+
 import geopandas as gpd
 from shapely.wkt import loads
 from shapely.geometry import Point
@@ -5,16 +7,41 @@ from shapely.geometry import Point
 from src.components.graph import SubDistrict, Stop
 
 
-def match_stops_to_subdistricts(stops: list[Stop], subdistricts: list[SubDistrict], crs="EPSG:4326"):
+def match_stops_to_subdistricts(stops: list[Stop], subdistricts: list[SubDistrict], buffer_metres: int = 0, crs="EPSG:4326"):
     """
-    Match Stop objects (points) to SubDistrict objects (polygons).
+    Match each Stops (points) to all SubDistricts (polygons) that either surround the stop or are within buffer_metres
+    of the point. The function drops stops that have no matching subdistricts.
 
     Args:
         stops (list[Stop]): list of Stop objects
         subdistricts (list[SubDistrict]): list of SubDistrict objects
+        buffer_metres (int): The tolerance distance that a stop might be outside a district and still be counted as within it
         crs (str): coordinate reference system (default EPSG:4326)
 
     Returns:
-        GeoDataFrame: each stop with its matched subdistrict ID (if any)
+        GeoDataFrame: each stop with its matched subdistrict ID.
+        Stops that don't correspond to any subdistrict are dropped.
     """
-    return []
+
+    if buffer_metres < 0:
+        raise ValueError("The buffer distance must be positive!")
+
+    subdistricts_geoframe = gpd.GeoDataFrame(
+        {"subdistrict_id": [sd.id for sd in subdistricts]},
+        geometry=[loads(sd.shape) for sd in subdistricts],
+        crs=crs
+    ).to_crs("EPSG:3857") # convert to 'Web Mercator' projection where coordinates are in metres
+
+    stop_geoframe = gpd.GeoDataFrame(
+        {"stop_id": [s.id for s in stops]},
+        geometry=[Point(s.lon, s.lat) for s in stops],  # Point(x=lon, y=lat)
+        crs=crs
+    ).to_crs("EPSG:3857") # convert to 'Web Mercator' projection where coordinates are in metres
+
+    # Spatial join: Match each stop with all subdistricts that either surround the stop or are within buffer_metres.
+    # Beware: inner join -> drops points that are within no district
+    matches = gpd.sjoin(stop_geoframe, subdistricts_geoframe, how="inner", predicate="dwithin", distance=buffer_metres)
+
+    # Group into (stop_id, [list of subdistricts])
+    grouped = matches.groupby("stop_id", sort=False)["subdistrict_id"].agg(list)
+    return grouped.items()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.16"
+__generated_with = "0.15.2"
 app = marimo.App(width="medium")
 
 
@@ -8,44 +8,31 @@ app = marimo.App(width="medium")
 def imports():
     import components.graph as graph
     import components.geo_spatial as geo
+    return geo, graph
 
-    return graph, geo
 
 @app.cell
 def import_city_data(graph):
     graph.import_city_data()
     return
 
+
 @app.cell
-def match_stops_with_districts(graph, geo):
+def match_stops_with_districts(geo, graph):
     stops = graph.get_stops()
     print(f"Queried {len(stops)} stops from the graph")
     subdistricts = graph.get_subdistricts()
     print(f"Queried {len(subdistricts)} subdistricts from the graph")
 
-    matched_stops = geo.match_stops_to_subdistricts(stops, subdistricts)
-    print(matched_stops)
+    stops_within_districts = geo.match_stops_to_subdistricts(stops, subdistricts, buffer_metres = 50)
+    for stop in stops_within_districts:
+        print(stop)
 
+    stops_close_to_districts = geo.match_stops_to_subdistricts(stops, subdistricts, buffer_metres = 500)
+    for stop in stops_close_to_districts:
+        print(stop)
+    return
 
-# @app.cell
-# def _():
-#     import pathlib as pl
-#
-#     # import pandas as pd
-#     # import geopandas as gp
-#     # import folium as fl
-#
-#     import gtfs_kit as gtfs
-#
-#     DATA = pl.Path("./data")
-#     return DATA, gtfs
-
-
-# @app.cell
-# def _(DATA, gtfs):
-#     path = DATA / "wiener_linien_gtfs.zip"
-#     gtfs.list_feed(path)
-#     return (path,)
 
 if __name__ == "__main__":
     app.run()
