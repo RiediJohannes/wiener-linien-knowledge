8f461458291b0b5b412f28c7b55747d7ec9304b7
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2025-09-01 21:18:01 +0200

    Added some text and thus overall structure to the notebook

--- src/main.py
+++ src/main.py
@@ -6,14 +6,65 @@ app = marimo.App(width="medium")
 
 @app.cell
 def imports():
+    import marimo as mo
+
     import components.graph as graph
     import components.geo_spatial as geo
-    return geo, graph
+    return geo, graph, mo
+
+
+@app.cell
+def _(mo):
+    mo.callout(mo.md("""
+    # 🚊 Wiener Linien Knowledge Graph 🚋
+    """), kind='danger')
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    # Creating the knowledge graph
+    First, we need to load the data about Viennas registration districts into our knowledge graph. This includes information about the registration districts' naming, their population and area, as well as their geographic coordinates.
+    """
+    )
+    return
 
 
 @app.cell
 def import_city_data(graph):
     graph.import_city_data()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+    # Knowledge graph evolution
+    Next, we obtain new knowledge from our data.
+
+    ## Merging nearby stops
+    In the first step, we detect all groups of nearby stops that practically function like one single stop.
+    """
+    )
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    ## Matching stops to districts
+    Next, we match each stop to the Viennese registration districts that either contain the stop or are reasonably close to the stop.
+    """
+    )
     return
 
 
